31bb52f7d2f1e64e3bd6a7ce33137ee822765c32
ozbe <1372945+ozbe@users.noreply.github.com>
2021-04-15 09:20:36 +0000

    fix spark not initializing in jupyter notebook

--- scripts/jupyter_start.sh
+++ scripts/jupyter_start.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+nohup /home/livy/bin/livy-server &
 /usr/local/bin/jupyter notebook --allow-root --NotebookApp.token='' --NotebookApp.password='' --no-browser --ip=0.0.0.0 --NotebookApp.allow_remote_access=True --NotebookApp.disable_check_xsrf=True
